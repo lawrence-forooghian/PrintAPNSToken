61493937a9c0107ca0c316c9b86778130ad6349e
Lawrence Forooghian <lawrence@forooghian.com>
2023-06-13 08:20:45 -0300

    Create basic app to register for push notifications and display token

--- PrintAPNSToken/ContentView.swift
+++ PrintAPNSToken/ContentView.swift
@@ -8,19 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var appDelegate: AppDelegate
+    @State private var authorizationResult: Result<Bool, Error>?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            if let authorizationResult {
+                switch authorizationResult {
+                case .success(true):
+                    EmptyView()
+                case .success(false):
+                    Text("Authorization not granted to display notifications.")
+                case .failure(let error):
+                    Text("Failed to get permission to display notifications: \(error.localizedDescription)")
+                }
+            }
+
+            switch appDelegate.remoteNotificationsRegistrationState {
+            case .inactive:
+                EmptyView()
+            case .registering:
+                Text("Registering for remote notifications…")
+                ProgressView()
+            case .terminated(.success(let tokenData)):
+                Text("Registered for remote notifications; device token is \(tokenData.base64EncodedString())")
+                ShareLink("Share Base64 device token", item: tokenData.base64EncodedString())
+            case .terminated(.failure(let error)):
+                Text("Failed to register for remote notifications: \(error.localizedDescription)")
+            }
+
+            Button(appDelegate.remoteNotificationsRegistrationState.isSuccess ? "Re-register for remote notifications" : "Register for remote notifications") {
+#if os(iOS)
+                Task { @MainActor in
+                    do {
+                        let granted = try await UNUserNotificationCenter.current().requestAuthorization(options: .alert)
+                        authorizationResult = .success(granted)
+                    } catch {
+                        authorizationResult = .failure(error)
+                        return
+                    }
+
+                    UIApplication.shared.registerForRemoteNotifications()
+                    appDelegate.remoteNotificationsRegistrationState = .registering
+                }
+#endif
+            }.disabled(appDelegate.remoteNotificationsRegistrationState.isRegistering)
         }
         .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
--- PrintAPNSToken/PrintAPNSTokenApp.swift
+++ PrintAPNSToken/PrintAPNSTokenApp.swift
@@ -6,12 +6,58 @@
 //
 
 import SwiftUI
+#if os(iOS)
+import UIKit
+#endif
 
 @main
 struct PrintAPNSTokenApp: App {
+#if os(iOS)
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+#endif
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+#if os(iOS)
+class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    @Published var remoteNotificationsRegistrationState: RemoteNotificationsRegistrationState = .inactive
+
+    enum RemoteNotificationsRegistrationState {
+        case inactive
+        case registering
+        case terminated(Result<Data, Error>)
+
+        var isRegistering: Bool {
+            if case .registering = self {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        var isSuccess: Bool {
+            if case .terminated(.success) = self {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        remoteNotificationsRegistrationState = .terminated(.success(deviceToken))
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        remoteNotificationsRegistrationState = .terminated(.failure(error))
+    }
+}
+#endif
