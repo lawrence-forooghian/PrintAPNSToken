5ff6ba774750559d8f1ea91e4cebb936ae78b6a3
Lawrence Forooghian <lawrence@forooghian.com>
2023-06-13 07:51:14 -0300

    Initial Commit

---
+++ PrintAPNSToken/PrintAPNSTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrintAPNSTokenApp.swift
+//  PrintAPNSToken
+//
+//  Created by Lawrence Forooghian on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrintAPNSTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
